a0e39f147f05377bfdc36949a24ffcf2e64dff7d
aosang <wangle2071@163.com>
2025-03-16 20:57:50 +0800

    v202503162057

--- itAssetsMobile/pages/home/home.vue
+++ itAssetsMobile/pages/home/home.vue
@@ -1,51 +1,15 @@
 <template>
 	<view class="content">
 		<view class="text-area">
-			<text class="title">{{title}}</text>
+			<uv-button type="success" text="确定"></uv-button>
 		</view>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello World'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+<script setup>
+	
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+	
 </style>
